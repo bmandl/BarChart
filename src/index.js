3146441b55e6bd18aa02e985c6065aa86b2870d7
Blaž Mandl <blaz.mandl@gmail.com>
2019-08-13 21:09:58 +0200

    Finished x-axis and scale with scaleTime. Changed json parsing to use d3 library. Selecting dom elements with d3 library functions

--- src/index.js
+++ src/index.js
@@ -9,54 +9,57 @@ const svg = d3.select(".center")
     .attr("height", h)
     .attr("id", "svg");
 
-const xScale = d3.scaleLinear();
+const xScale = d3.scaleTime();
 const yScale = d3.scaleLinear();
 
-var req = new XMLHttpRequest();
-req.open("GET", "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json", true);
-req.send();
-req.onload = () => {
-    const json = JSON.parse(req.responseText);
-    const dataset = json.data;
-    const w = (document.getElementById("svg").clientWidth - padding) / dataset.length;
+const dateRange = dataset => {
 
-    xScale.domain([0,dataset.length-1]);
-    xScale.range([padding,w*dataset.length]);
+    const min = d3.min(dataset, d => new Date(d[0])),
+        max = d3.max(dataset, d => new Date(d[0]));
+
+    return { min, max };
+}
+
+d3.json('https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json').then(data => {
+
+    const dataset = data.data;
+
+    const w = (d3.select("#svg").node().clientWidth - padding) / dataset.length;
+
+    const xDomain = dateRange(dataset);
+
+    xScale.domain([xDomain.min, xDomain.max]);
+    xScale.range([padding, w * dataset.length]);
 
     yScale.domain([0, d3.max(dataset, d => d[1])]);
-    yScale.range([h - padding, padding]);//800 / 10
+    yScale.range([h - padding, padding]);
 
     const xAxis = d3.axisBottom(xScale);
     const yAxis = d3.axisLeft(yScale);
-    
+
     svg.selectAll("rect")
         .data(dataset)
         .enter()
         .append("rect")
-        .attr("x", (d, i) => xScale(i))
+        .attr("x", d => xScale(new Date(d[0])))
         .attr("y", d => yScale(d[1]))
-        .attr("rx",2)
-        //.attr("ry",1)
+        .attr("rx", 2)
         .attr("width", w)
-        .attr("height", d => h - yScale(d[1])-padding)
+        .attr("height", d => h - yScale(d[1]) - padding)
         .attr("fill", "navy")
         .attr("class", "bar")
+        .attr("data-date", d => d[0])
+        .attr("data-gdp", d => d[1])
         .append("title")
         .text(d => d[1]);
 
     svg.append("g")
-        .attr("transform", `translate(0,${(h-padding)})`)
+        .attr("id", "x-axis")
+        .attr("transform", `translate(0,${(h - padding)})`)
         .call(xAxis);
 
     svg.append("g")
-       .attr("transform", `translate(${(padding)},0)`)
-       .call(yAxis);
-
-    /*svg.selectAll("text")
-        .data(dataset)
-        .enter()
-        .append("text")
-        .attr("x",(d,i) => i * (w+2))
-        .attr("y", d => h - scale(d[1]) / 2)
-        .text(d => d[1]);*/
-}
+        .attr("id", "y-axis")
+        .attr("transform", `translate(${(padding)},0)`)
+        .call(yAxis);
+});
